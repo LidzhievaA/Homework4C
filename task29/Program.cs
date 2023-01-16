fbbc3ccce98316aabda347be38716afe5f404607
Alsu Lidzhieva <magadieva.alsu@yandex.ru>
2023-01-16 16:59:46 +0500

    Добавили решение Task 29

--- task29/Program.cs
+++ task29/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-//int[] array = new int[8];
 
 Console.WriteLine("Введите значение наименьшего элемента массива: ");
 int minNumber = Convert.ToInt32(Console.ReadLine());
@@ -26,8 +25,12 @@ return arr;
 
 void PrintArray(int[] arr)
 {
+Console.Write("[");
 for (int i = 0; i < arr.Length; i++)
 {
-Console.Write(arr[i] + " ");
+Console.Write(arr[i] + " " );
 }
+Console.Write("]");
 }
+
+
